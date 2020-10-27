7ea4ddf60635d1e46ffda6d2d8a256c8d81d58e1
jrandersen-ga <andersen.ja@gmail.com>
2020-10-26 23:30:27 -0400

    adds tasks route with plavceholder data, adds navbar url routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+class Task:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, project):
+    self.name = name
+    self.project = project
+
+basic_tasks = [
+  Task('Do this', 'client-one'),
+  Task('Do that', 'client-one'),
+  Task('Do something', 'client-one'),
+]
+
+
 # Create your views here.
 from django.http import HttpResponse
 
@@ -9,3 +21,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def tasks(request):
+  context = { 'basic_tasks': basic_tasks }
+  return render(request, 'tasks/index.html', context )
