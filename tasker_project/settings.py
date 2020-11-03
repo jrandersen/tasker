9ce29626ff55ecfc2b6b8add7d9d05cde45a0e39
jrandersen-ga <andersen.ja@gmail.com>
2020-11-03 12:23:42 -0500

    takes out template_dirs

--- tasker_project/settings.py
+++ tasker_project/settings.py
@@ -24,7 +24,6 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -66,9 +65,7 @@ ROOT_URLCONF = 'tasker_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
