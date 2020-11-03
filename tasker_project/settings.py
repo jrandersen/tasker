c971975bac7bdba5e5a47b6d4f41b7e4a2e80213
jrandersen-ga <andersen.ja@gmail.com>
2020-11-03 10:41:36 -0500

    adds heroku deploy config

--- tasker_project/settings.py
+++ tasker_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,5 @@ LOGOUT_REDIRECT_URL = '/'
 
 # SIGNUP REDIRECTS TO ===============
 SIGNUP_REDIRECT_URL = '/'
+
+django_on_heroku.settings(locals())
